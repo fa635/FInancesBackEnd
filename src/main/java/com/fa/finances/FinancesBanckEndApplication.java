88ee34f8f3c263db976585c4879fe304d321df5a
fa <bfalonne823@gmail.com>
2025-10-09 19:03:14 +0200

    user model

---
+++ src/main/java/com/fa/finances/FinancesBanckEndApplication.java
@@ -0,0 +1,13 @@
+package com.fa.finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesBanckEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesBanckEndApplication.class, args);
+	}
+
+}
